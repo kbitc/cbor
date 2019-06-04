8f61afd7bdf25ebd235f4ce4cb1ea1185bb4a322
Richard King <47585928+kbitc@users.noreply.github.com>
2019-06-05 00:13:11 +0800

    Update cbor_encoder.c

--- src/cbor_encoder.c
+++ src/cbor_encoder.c
@@ -28,421 +28,226 @@
 #include <math.h>
 #include <stdint.h>
 #include <string.h>
-#include <stdlib.h>
 
-cbor_t *cbor_create(size_t size)
+bool ensure_capacity(uint8_t *buf, size_t size, size_t offset)
 {
-	cbor_t *cbor = (cbor_t *)malloc(sizeof(cbor_t));
-	if (cbor != NULL)
-	{
-		cbor->buf = (uint8_t *)malloc(sizeof(uint8_t) * size);
-		if (cbor->buf == NULL)
-		{
-			free(cbor);
-			cbor = NULL;
-		}
-		else
-		{
-			cbor->offset = 0;
-			cbor->size = size;
-		}
-	}
-	return cbor;
+	return size >= offset;
 }
 
-void cbor_free(cbor_t *cbor)
+static int __cbor_encode_uint(uint8_t *buf, size_t size, size_t *pos, uint8_t ib_mt, uint64_t val)
 {
-	if (cbor != NULL)
+	size_t len = (val <= 23) ? 0 \
+		: (val <= 255) ? 1 \
+		: (val <= 65535) ? 2 \
+		: (val <= 4294967295) ? 4 \
+		: 8;
+	if (!ensure_capacity(buf, size, *pos + len + 1))
 	{
-		if (cbor->buf != NULL)
-		{
-			free(cbor->buf);
-		}
-		free(cbor);
+		return CBOR_ERR_OUT_OF_MEMORY;
 	}
+
+	if (len == 0)
+	{
+		buf[*pos] = ib_mt | val;
+	}
+	else if (len == 1)
+	{
+		buf[*pos] = ib_mt | AI_1;
+		buf[++*pos] = val;
+	}
+	else if (len == 2)
+	{
+		buf[*pos] = ib_mt | AI_2;
+		stonb((uint16_t)val, buf + ++*pos);
+	}
+	else if (len == 5)
+	{
+		buf[*pos] = ib_mt | AI_4;
+		ltonb((uint32_t)val, buf + ++*pos);
+	}
+	else // if (len == 8)
+	{
+		buf[*pos] = ib_mt | AI_8;
+		lltonb(val, buf + ++*pos);
+	}
+
+	*pos += len;
+	return CBOR_NO_ERROR;
 }
 
-__attribute__((weak)) bool ensure_capacity(cbor_t *cbor, size_t size)
+static int __cbor_encode_bytes(uint8_t *buf, size_t size, size_t *pos, uint8_t ib_mt, const void *bytes, size_t len)
 {
-	return cbor->offset + size <= cbor->size;
+	int ret = __cbor_encode_uint(buf, size, pos, ib_mt, len);
+	if (ret != CBOR_NO_ERROR)
+	{
+		return ret;
+	}
+
+	if (ensure_capacity(buf, size, *pos + len))
+	{
+		memcpy(buf + *pos, bytes, len);
+		*pos += len;
+	}
+	return CBOR_ERR_OUT_OF_MEMORY;
 }
 
-static bool __cbor_create_int(cbor_t *cbor, int64_t val, bool offsetitive)
+static int __cbor_encode_indef(uint8_t *buf, size_t size, size_t *pos, uint8_t ib_mt)
 {
-	uint64_t uv = (uint64_t)val;
-	uint8_t ib_mt = IB_UINT;
-	if (!offsetitive && (val & 0x8000000000000000))
+	if (ensure_capacity(buf, size, *pos + 2))
 	{
-		uv = ~uv;
-		ib_mt = IB_NEGINT;
+		buf[(*pos)++] = ib_mt | AI_INDEF;
+		return CBOR_NO_ERROR;
 	}
-
-	if (uv <= 23)
-	{
-		if (ensure_capacity(cbor, 1))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | uv;
-			return true;
-		}
-	}
-	else if (uv <= 255)
-	{
-		if (ensure_capacity(cbor, 2))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_1;
-			cbor->buf[cbor->offset++] = uv;
-			return true;
-		}
-	}
-	else if (uv <= 65535)
-	{
-		if (ensure_capacity(cbor, 3))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_2;
-			stonb((uint16_t)uv, cbor->buf + cbor->offset);
-			cbor->offset += 2;
-			return true;
-		}
-	}
-	else if (uv <= 4294967295)
-	{
-		if (ensure_capacity(cbor, 5))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_4;
-			ltonb((uint32_t)uv, cbor->buf + cbor->offset);
-			cbor->offset += 4;
-			return true;
-		}
-	}
-	else
-	{
-		if (ensure_capacity(cbor, 9))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_8;
-			lltonb(uv, cbor->buf + cbor->offset);
-			cbor->offset += 8;
-			return true;
-		}
-	}
-	return false;
+	return CBOR_ERR_OUT_OF_MEMORY;
 }
 
-bool cbor_set_int(cbor_t *cbor, int64_t val)
+// 1. encode signed integer
+int cbor_encode_int(uint8_t *buf, size_t size, size_t *pos, int64_t val)
 {
-	return __cbor_create_int(cbor, val, false);
+	return (val & 0x8000000000000000) \
+		? __cbor_encode_uint(buf, size, pos, IB_NEGINT, ~val) \
+		: __cbor_encode_uint(buf, size, pos, IB_UINT, val);
 }
 
-bool cbor_set_uint(cbor_t *cbor, uint64_t val)
+// 2. encode unsigned integer
+int cbor_encode_uint(uint8_t *buf, size_t size, size_t *pos, uint64_t val)
 {
-	return __cbor_create_int(cbor, val, true);
+	return __cbor_encode_uint(buf, size, pos, IB_UINT, val);
 }
 
-
-static bool __cbor_create_bytes(cbor_t *cbor, const uint8_t *bytes, size_t len, uint8_t ib_mt)
+// 3. encode tag
+int cbor_encode_tag(uint8_t *buf, size_t size, size_t *pos, uint64_t val)
 {
-	if (len <= 23)
+	return __cbor_encode_uint(buf, size, pos, IB_TAG, val);
+}
+
+// 4. encode simple
+int cbor_encode_simple(uint8_t *buf, size_t size, size_t *pos, uint8_t val)
+{
+	if (val > 23 && val < 32)
 	{
-		if (ensure_capacity(cbor, 1 + len))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | len;
-			memcpy(cbor->buf + cbor->offset, bytes, len);
-			cbor->offset += len;
-			return true;
-		}
+		return CBOR_ERR_SIMPLE_OUT_OF_SCOPE;
 	}
-	else if (len <= 255)
+	return __cbor_encode_uint(buf, size, pos, IB_PRIM, val);
+}
+
+// 5. encode double, float and half
+int cbor_encode_float(uint8_t *buf, size_t size, size_t *pos, double val)
+{
+	if (!ensure_capacity(buf, size, *pos + 3))
 	{
-		if (ensure_capacity(cbor, 2 + len))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_1;
-			cbor->buf[cbor->offset++] = len;
-			memcpy(cbor->buf + cbor->offset, bytes, len);
-			cbor->offset += len;
-			return true;
-		}
+		return CBOR_ERR_OUT_OF_MEMORY;
 	}
-	else if (len <= 65535)
-	{
-		if (ensure_capacity(cbor, 3 + len))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_2;
-			stonb(len, cbor->buf + cbor->offset);
-			cbor->offset += 2;
-			memcpy(cbor->buf + cbor->offset, bytes, len);
-			cbor->offset += len;
-			return true;
-		}
-	}
-	else if (len <= 4294967295)
-	{
-		if (ensure_capacity(cbor, 5 + len))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_4;
-			ltonb(len, cbor->buf + cbor->offset);
-			cbor->offset += 4;
-			memcpy(cbor->buf + cbor->offset, bytes, len);
-			cbor->offset += len;
-			return true;
-		}
-	}
-	else
-	{
-		if (ensure_capacity(cbor, 9 + len))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_8;
-			lltonb(len, cbor->buf + cbor->offset);
-			cbor->offset += 8;
-			memcpy(cbor->buf + cbor->offset, bytes, len);
-			cbor->offset += len;
-			return true;
-		}
-	}
-	return false;
-}
 
-bool cbor_set_bytes(cbor_t *cbor, const uint8_t *bytes, size_t len)
-{
-	return __cbor_create_bytes(cbor, bytes, len, IB_BYTES);
-}
-
-bool cbor_set_string(cbor_t *cbor, const char *str)
-{
-	return __cbor_create_bytes(cbor, (const uint8_t *)str, strlen(str), IB_STRING);
-}
-
-
-bool cbor_begin_bytes(cbor_t *cbor)
-{
-	if (ensure_capacity(cbor, 1))
-	{
-		cbor->buf[cbor->offset++] = IB_BYTES | AI_INDEF;
-		return true;
-	}
-	return false;
-}
-
-bool cbor_end_bytes(cbor_t *cbor)
-{
-	if (ensure_capacity(cbor, 1))
-	{
-		cbor->buf[cbor->offset++] = AI_BRKCD;
-		return true;
-	}
-	return false;
-}
-
-bool cbor_begin_string(cbor_t *cbor)
-{
-	if (ensure_capacity(cbor, 1))
-	{
-		cbor->buf[cbor->offset++] = IB_STRING | AI_INDEF;
-		return true;
-	}
-	return false;
-}
-
-bool cbor_end_string(cbor_t *cbor)
-{
-	if (ensure_capacity(cbor, 1))
-	{
-		cbor->buf[cbor->offset++] = AI_BRKCD;
-		return true;
-	}
-	return false;
-}
-
-
-static bool __cbor_create_set(cbor_t *cbor, size_t len, uint8_t ib_mt)
-{
-	if (len <= 23)
-	{
-		if (ensure_capacity(cbor, 1))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | len;
-			return true;
-		}
-	}
-	else if (len <= 255)
-	{
-		if (ensure_capacity(cbor, 2))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_1;
-			cbor->buf[cbor->offset++] = len;
-			return true;
-		}
-	}
-	else if (len <= 65535)
-	{
-		if (ensure_capacity(cbor, 3))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_2;
-			stonb(len, cbor->buf + cbor->offset);
-			cbor->offset += 2;
-			return true;
-		}
-	}
-	else if (len <= 4294967295)
-	{
-		if (ensure_capacity(cbor, 5))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_4;
-			ltonb(len, cbor->buf + cbor->offset);
-			cbor->offset += 4;
-			return true;
-		}
-	}
-	else
-	{
-		if (ensure_capacity(cbor, 9))
-		{
-			cbor->buf[cbor->offset++] = ib_mt | AI_8;
-			lltonb(len, cbor->buf + cbor->offset);
-			cbor->offset += 8;
-			return true;
-		}
-	}
-	return false;
-}
-
-bool cbor_set_array(cbor_t *cbor, size_t len)
-{
-	return __cbor_create_set(cbor, len, IB_ARRAY);
-}
-
-bool cbor_set_map(cbor_t *cbor, size_t len)
-{
-	return __cbor_create_set(cbor, len, IB_MAP);
-}
-
-static bool __cbor_begin_set(cbor_t *cbor, uint8_t ib_mt)
-{
-	if (ensure_capacity(cbor, 1))
-	{
-		cbor->buf[cbor->offset++] = ib_mt | AI_INDEF;
-		return true;
-	}
-	return false;
-}
-
-static bool __cbor_end_set(cbor_t *cbor)
-{
-	if (ensure_capacity(cbor, 1))
-	{
-		cbor->buf[cbor->offset++] = AI_BRKCD;
-		return true;
-	}
-	return false;
-}
-
-bool cbor_begin_array(cbor_t *cbor)
-{
-	return __cbor_begin_set(cbor, IB_ARRAY);
-}
-
-bool cbor_end_array(cbor_t *cbor)
-{
-	return __cbor_end_set(cbor);
-}
-
-bool cbor_begin_map(cbor_t *cbor)
-{
-	return __cbor_begin_set(cbor, IB_MAP);
-}
-
-bool cbor_end_map(cbor_t *cbor)
-{
-	return __cbor_end_set(cbor);
-}
-
-
-bool cbor_set_float(cbor_t *cbor, double val)
-{
 	if (val == 0.0)									// 0.0, -0.0
 	{
-		if (ensure_capacity(cbor, 3))
-		{
-			cbor->buf[cbor->offset++] = IB_PRIM | AI_2;
-			cbor->buf[cbor->offset++] = 0x00;
-			cbor->buf[cbor->offset++] = 0x00;
-			return true;
-		}
-	}
-	else if (!isfinite(val))						// Infinity
-	{
-		if (ensure_capacity(cbor, 3))
-		{
-			cbor->buf[cbor->offset++] = IB_PRIM | AI_2;
-			cbor->buf[cbor->offset++] = 0x7c;
-			cbor->buf[cbor->offset++] = 0x00;
-			return true;
-		}
+		buf[(*pos)++] = IB_PRIM | AI_2;
+		stonb(0x0000, buf + *pos);
+		*pos += 2;
 	}
 	else if (isnan(val))							// NaN
 	{
-		if (ensure_capacity(cbor, 3))
-		{
-			cbor->buf[cbor->offset++] = IB_PRIM | AI_2;
-			cbor->buf[cbor->offset++] = 0x7e;
-			cbor->buf[cbor->offset++] = 0x00;
-			return true;
-		}
+		buf[(*pos)++] = IB_PRIM | AI_2;
+		stonb(0x7e00, buf + *pos);
+		*pos += 2;
 	}
-	
-	float fval = (float)val;
-	if (fval != val)								// double
+	else if (!isfinite(val))						// Infinity
 	{
-		if (ensure_capacity(cbor, 9))
-		{
-			cbor->buf[cbor->offset++] = IB_PRIM | AI_8;
-			dtonb(val, cbor->buf + cbor->offset);
-			cbor->offset += 8;
-			return true;
-		}
+		buf[(*pos)++] = IB_PRIM | AI_2;
+		stonb(0x7c00, buf + *pos);
+		*pos += 2;
 	}
-	else if (is_ftoh_loss(fval))					// float
+	else
 	{
-		if (ensure_capacity(cbor, 5))
+		float fval = (float)val;
+		if (fval != val)								// double
 		{
-			cbor->buf[cbor->offset++] = IB_PRIM | AI_4;
-			ftonb(fval, cbor->buf + cbor->offset);
-			cbor->offset += 4;
-			return true;
+			if (!ensure_capacity(buf, size, *pos + 9))
+			{
+				return CBOR_ERR_OUT_OF_MEMORY;
+			}
+
+			buf[(*pos)++] = IB_PRIM | AI_8;
+			dtonb(val, buf + *pos);
+			*pos += 8;
 		}
-	}
-	else											// float16
-	{
-		if (ensure_capacity(cbor, 3))
+		else if (is_ftoh_loss(fval))					// float
+		{
+			if (!ensure_capacity(buf, size, *pos + 5))
+			{
+				return CBOR_ERR_OUT_OF_MEMORY;
+			}
+
+			buf[(*pos)++] = IB_PRIM | AI_4;
+			ftonb(fval, buf + *pos);
+			*pos += 4;
+		}
+		else											// float16
 		{
 			half hval = ftoh(fval);
-			cbor->buf[cbor->offset++] = IB_PRIM | AI_2;
-			htonb(hval, cbor->buf + cbor->offset);
-			cbor->offset += 2;
-			return true;
+			buf[(*pos)++] = IB_PRIM | AI_2;
+			htonb(hval, buf + *pos);
+			*pos += 2;
 		}
 	}
-
-	return false;
+	return CBOR_NO_ERROR;
 }
 
-
-bool cbor_set_simple(cbor_t *cbor, uint8_t val)
+// 6. encode bytes
+int cbor_encode_bytes(uint8_t *buf, size_t size, size_t *pos, const uint8_t *bytes, size_t len)
 {
-	if (val <= 23 || val == AI_INDEF)
+	return __cbor_encode_bytes(buf, size, pos, IB_BYTES, bytes, len);
+}
+
+// 7. encode indefinite-length bytes
+int cbor_encode_bytes_indef(uint8_t *buf, size_t size, size_t *pos)
+{
+	return __cbor_encode_indef(buf, size, pos, IB_BYTES);
+}
+
+// 8. encode string
+int cbor_encode_string(uint8_t *buf, size_t size, size_t *pos, const char *str)
+{
+	return __cbor_encode_bytes(buf, size, pos, IB_STRING, str, strlen(str));
+}
+
+// 9. encode indefinite-length string
+int cbor_encode_string_indef(uint8_t *buf, size_t size, size_t *pos)
+{
+	return __cbor_encode_indef(buf, size, pos, IB_STRING);
+}
+
+// 10. encode array
+int cbor_encode_array(uint8_t *buf, size_t size, size_t *pos, size_t len)
+{
+	return __cbor_encode_uint(buf, size, pos, IB_ARRAY, len);
+}
+
+// 11. encode indefinite-length array
+int cbor_encode_array_indef(uint8_t *buf, size_t size, size_t *pos, size_t len)
+{
+	return __cbor_encode_indef(buf, size, pos, IB_ARRAY);
+}
+
+// 12. encode map
+int cbor_encode_map(uint8_t *buf, size_t size, size_t *pos, size_t len)
+{
+	return __cbor_encode_uint(buf, size, pos, IB_MAP, len << 1);
+}
+
+// 13. encode indefinite-length map
+int cbor_encode_map_indef(uint8_t *buf, size_t size, size_t *pos, size_t len)
+{
+	return __cbor_encode_indef(buf, size, pos, IB_MAP);
+}
+
+// 14. encode break code
+int cbor_encode_break(uint8_t *buf, size_t size, size_t *pos, const uint8_t *bytes, size_t len)
+{
+	if (ensure_capacity(buf, size, *pos + 1))
 	{
-		if (ensure_capacity(cbor, 1))
-		{
-			cbor->buf[cbor->offset++] = IB_PRIM | val;
-			return true;
-		}
+		buf[(*pos)++] = AI_BRKCD;
+		return CBOR_NO_ERROR;
 	}
-	else if (val >=32)
-	{
-		if (ensure_capacity(cbor, 2))
-		{
-			cbor->buf[cbor->offset++] = IB_PRIM | AI_1;
-			cbor->buf[cbor->offset++] = val;
-			return true;
-		}
-	}
-	return false;
+	return CBOR_ERR_OUT_OF_MEMORY;
 }
