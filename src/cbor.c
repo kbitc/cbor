38fe2e622910bcb4f7b24499ae55187ef8d984ed
Richard King <47585928+kbitc@users.noreply.github.com>
2019-06-05 19:37:43 +0800

    Update cbor.c

--- src/cbor.c
+++ src/cbor.c
@@ -47,3 +47,146 @@ void cbor_free(cbor_t *cbor)
 		free(cbor);
 	}
 }
+
+int cbor_bytes_len(cbor_t *cbor, size_t *len)
+{
+	if (cbor->ct == CBOR_BYTES || cbor->ct == CBOR_STRING)
+	{
+		*len = cbor->size;
+		return CBOR_NO_ERROR;
+	}
+	else if (cbor->ct == CBOR_BYTES_INDEF || cbor->ct == CBOR_STRING_INDEF)
+	{
+		*len = 0;
+		for (size_t i = 0, pos = 0; i < cbor->count; i++)
+		{
+			cbor_t item;
+			int ret = cbor_decode(cbor->v.bytes, cbor->size, &pos, &item);
+			if (ret != CBOR_NO_ERROR)
+			{
+				return ret;
+			}
+
+			*len += item.size;
+		}
+		return CBOR_NO_ERROR;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int cbor_bytes_cmp(cbor_t *cbor, const void *buf, size_t size, int *res)
+{
+	if (cbor->ct == CBOR_BYTES || cbor->ct == CBOR_STRING)
+	{
+		size_t len = cbor->size > size ? size : cbor->size;
+		*res = memcmp(cbor->v.bytes, buf, len);
+		if (!*res)
+		{
+			*res = cbor->size - size;
+		}
+		return CBOR_NO_ERROR;
+	}
+	else if (cbor->ct == CBOR_BYTES_INDEF || cbor->ct == CBOR_STRING_INDEF)
+	{
+		for (size_t i = 0, pos1 = 0, pos2 = 0; i < cbor->count && pos2 < size; i++)
+		{
+			cbor_t item;
+			int ret = cbor_decode(cbor->v.bytes, cbor->size, &pos1, &item);
+			if (ret != CBOR_NO_ERROR)
+			{
+				return ret;
+			}
+
+			ret = cbor_bytes_cmp(&item, buf + pos2, item.size, res);
+			if (ret != CBOR_NO_ERROR)
+			{
+				return ret;
+			}
+
+			if (*res)
+			{
+				break;
+			}
+			pos2 += item.size;
+		}
+		return CBOR_NO_ERROR;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int cbor_array_get(cbor_t *cbor, size_t index, cbor_t *val)
+{
+	if (cbor->ct != CBOR_ARRAY)
+	{
+		return -1;
+	}
+
+	if (index >= cbor->count)
+	{
+		return -2;
+	}
+
+	size_t pos = 0;
+	for (size_t i = 0; i < index; i++)
+	{
+		int ret = cbor_verify(cbor->v.bytes, cbor->size, &pos);
+		if (ret != CBOR_NO_ERROR)
+		{
+			return ret;
+		}
+	}
+	return cbor_decode(cbor->v.bytes, cbor->size, &pos, val);
+}
+
+int cbor_map_get(cbor_t *cbor, const char *key, cbor_t *val)
+{
+	if (cbor->ct != CBOR_MAP)
+	{
+		return -1;
+	}
+
+	for (size_t i = 0, pos = 0; i < cbor->count; i += 2)
+	{
+		cbor_t _key;
+		int ret = cbor_decode(cbor->v.bytes, cbor->size, &pos, &_key);
+		if (ret != CBOR_NO_ERROR)
+		{
+			return ret;
+		}
+
+		if (_key.ct == CBOR_STRING)
+		{
+			if (_key.size == strlen(key) && !memcmp(_key.v.str, key, _key.size))
+			{
+				return cbor_decode(cbor->v.bytes, cbor->size, &pos, val);
+			}
+		}
+		else if (_key.ct == CBOR_STRING_INDEF)
+		{
+			int res = 0;
+			ret = cbor_bytes_cmp(&_key, key, strlen(key), &res);
+			if (ret != CBOR_NO_ERROR)
+			{
+				return ret;
+			}
+
+			if (!res)
+			{
+				return cbor_decode(cbor->v.bytes, cbor->size, &pos, val);
+			}
+		}
+
+		ret = cbor_verify(cbor->v.bytes, cbor->size, &pos);
+		if (ret != CBOR_NO_ERROR)
+		{
+			return ret;
+		}
+	}
+	return -2;
+}
